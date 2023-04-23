3a82f7f11cb9f4f2228e84319649dbf3ca297e1c
Jan <jazevo@web.de>
2023-04-23 16:49:13 +0200

    feat (braingpt): execute steps, todo: create final summary/answer to prompt, handle token limit

--- code/src/interfaces.ts
+++ code/src/interfaces.ts
@@ -1,9 +1,9 @@
 export interface Approach {
   approach: string;
   steps?: Step[];
-}
+};
 
 export interface Step {
   step: string;
   steps?: Step[];
-}
+};
--- code/src/promptchains/braingpt.ts
+++ code/src/promptchains/braingpt.ts
@@ -20,14 +20,14 @@ Each step of this conversation tries to enhance the initial prompt and get close
   public async chain(prompt): Promise<string[]> {
     this.resetChatMessages();
     this.initialPrompt = prompt;
-
     await this.generateContextIdeas();
     await this.generateContext();
     await this.generateApproaches();
     await this.filterApproaches();
     await this.generateSteps();
     await this.determineBestApproach();
-    await this.executeApproaches();
+    // this.bestApproach = {"approach":"Analyze current trends in AI research and development to determine the rate of progress towards achieving general intelligence.","steps":[{"step":"Gather data on current AI research and development trends.","steps":[{"step":"Identify reputable sources of information on AI research and development."},{"step":"Collect data on the current state of AI technology, including recent breakthroughs and advancements."},{"step":"Research the most promising areas of AI research, such as deep learning, natural language processing, and computer vision."},{"step":"Analyze funding trends in AI research and development to determine which areas are receiving the most investment."},{"step":"Consider the ethical implications of AI research and development, including issues related to bias, privacy, and job displacement."}]},{"step":"Analyze the rate of progress towards achieving general intelligence based on this data.","steps":[{"step":"Identify current AI research and development trends related to achieving general intelligence."},{"step":"Evaluate recent breakthroughs and advancements in AI that contribute to progress towards achieving general intelligence."},{"step":"Assess the limitations and obstacles that are slowing down progress towards achieving general intelligence."},{"step":"Consider potential future breakthroughs or advancements that could accelerate progress towards achieving general intelligence."}]},{"step":"Consider any potential breakthroughs or advancements that could accelerate progress towards the singularity.","steps":[{"step":"Identify current AI research and development trends related to achieving general intelligence."},{"step":"Evaluate recent breakthroughs and advancements in AI that contribute to progress towards achieving general intelligence."},{"step":"Assess the limitations and obstacles that are slowing down progress towards achieving general intelligence."},{"step":"Consider potential future breakthroughs or advancements that could accelerate progress towards achieving general intelligence."}]},{"step":"Evaluate any potential obstacles or limitations that could slow down progress towards the singularity.","steps":[{"step":"Identify potential limitations or obstacles that could slow down progress towards achieving general intelligence."},{"step":"Evaluate the current state of technology and research in relation to these limitations or obstacles."},{"step":"Consider potential solutions or workarounds for these limitations or obstacles, such as new algorithms or hardware advancements."},{"step":"Assess the feasibility and timeline for implementing these solutions or workarounds."},{"step":"Determine the potential impact of these limitations or obstacles on the timeline for achieving the singularity."}]},{"step":"Use all available information to make an informed prediction about the likelihood of the technological singularity occurring before 2030, expressed as a percentage.","steps":[{"step":"Identify potential limitations or obstacles that could slow down progress towards achieving general intelligence."},{"step":"Evaluate the current state of technology and research in relation to these limitations or obstacles."},{"step":"Consider potential solutions or workarounds for these limitations or obstacles, such as new algorithms or hardware advancements."},{"step":"Assess the feasibility and timeline for implementing these solutions or workarounds."},{"step":"Determine the potential impact of these limitations or obstacles on the timeline for achieving the singularity."}]}]}
+    await this.executeBestApproach();
 
     const answer = this.messages;
     this.resetChatMessages();
@@ -172,7 +172,92 @@ Only output the single word string, nothing before or after the word.`
     this.bestApproach = this.approaches[number - 1];
   }
 
-  private async executeApproaches(): Promise<void> {
+  private async executeBestApproach(): Promise<void> {
+    const stepsArray = this.flattenApproach(this.bestApproach);
+    const resultsArray = [];
 
+    for (let i = 0; i < stepsArray.length; i++) {
+      const step = stepsArray[i];
+      const previousSteps = stepsArray.slice(0, i);
+      const stepsAndResults = previousSteps.map((s, j) => 'Step: ' + s + '\nAnswer: ' + resultsArray[j]);
+
+      if (!stepsAndResults.length) {  // 1st step
+        const result = await this.executeStep(step);
+        resultsArray.push(result);
+      } else {
+        const summary = await this.summarizeSteps(stepsAndResults, step, 3000);
+        const result = await this.executeStep(step, summary);
+        resultsArray.push(result);
+      }
+    }
+  }
+
+  // turn approach into 1D array of steps
+  private flattenApproach(approach: Approach, prefix: string = '', steps?: Step[]): string[] {
+    steps = steps || approach.steps;
+    const result: string[] = [];
+
+    for (let i = 0; i < steps.length; i++) {
+      const step = steps[i];
+      const stepNumber = `${prefix}${i + 1}`;
+      const stepString = `${stepNumber}. ${step.step}`;
+      if (step.steps) result.push(...this.flattenApproach(approach, `${stepNumber}.`, step.steps));
+      result.push(stepString);  // put 1 after 1.1 since usually the parent step is a summary of child steps -> better execution order
+    }
+
+    return result;
+  }
+
+  private async summarizeSteps(steps: string[], currentStep: string, limit: number): Promise<string> {
+    const extractPromptStart = `To solve the prompt "${this.initialPrompt}" this approach was generated: "${this.bestApproach.approach}".
+For this approach the following steps were generated:\n"`;
+
+    const extractPromptEnd = `"\nExtract the key takeaways or a summary from these steps that are needed to execute the following next step:\n${currentStep}
+Be concise. The summary will be provided to solve the next step. If there are no takeaways from these steps, output nothing.`;
+
+    const parts = [];
+    let lastJoined: string;
+    let lastPartIndex = 0;
+
+    steps.forEach((s, i) => {
+      const currentSteps = steps.slice(lastPartIndex, i + 1);
+      const joined = currentSteps.join('\n');
+      const tokenSum = this.countTokens(extractPromptStart + joined + extractPromptEnd);
+
+      if (tokenSum > limit) {
+        parts.push(lastJoined);
+        lastPartIndex = i;
+      }
+    });
+
+    if (!parts.length) parts.push(steps.join('\n'));
+
+    const extracts = await this.extractInfoFromPreviousSteps(parts, extractPromptStart, extractPromptEnd)
+    return extracts.join('\n'); // TODO: for now join extracts, but in future summarize extracts so that sum is below token limit
+  }
+
+  private async extractInfoFromPreviousSteps(parts: string[], extractPromptStart: string, extractPromptEnd: string): Promise<string[]> {
+    return Promise.all(parts.map(p => {
+      const message = extractPromptStart + p + extractPromptEnd;
+      return this.chatSingle(message);
+    }));
+  }
+
+  private async executeStep(step: string, summary?: string): Promise<string> {
+    let message: string;
+
+    if (summary) {
+      message = `To solve the prompt "${this.initialPrompt}" this approach was generated: "${this.bestApproach.approach}".
+You are given the following helpful information from previous steps:
+"${summary}".
+With this information, solve the current step "${step}". Output a concise and short answer/execution to this step and take into consideration the above information.
+If you can't answer this step, output nothing.`;
+    } else {
+      message = `To solve the prompt "${this.initialPrompt}" this approach was generated: "${this.bestApproach.approach}".
+Solve the step "${step}". Output a concise and short answer/execution to this step.
+If you can't answer this step, output nothing.`;
+    }
+
+    return this.chatSingle(message);
   }
 }
