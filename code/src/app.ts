233d98653041d78b8f03d4dbbcb341357606958e
Jan <jazevo@web.de>
2023-05-26 17:11:51 +0200

    set up https certificate for pi

--- code/src/app.ts
+++ code/src/app.ts
@@ -1,7 +1,11 @@
-require('dotenv').config();
+import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
+import https from 'https';
+import fs from 'fs';
 import routes from './routes';
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
@@ -10,6 +14,11 @@ app.use(cors());
 app.use(express.json());
 app.use('/', routes);
 
-app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
+const httpsOptions = {
+  key: fs.readFileSync('/etc/letsencrypt/live/promptchain.duckdns.org/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/promptchain.duckdns.org/fullchain.pem')
+};
+
+https.createServer(httpsOptions, app).listen(port, () => {
+  console.log(`Listening at https://localhost:${port}`);
 });
